caa3e5a5a352392785faec8f396608d6cbefa779
tomasfranquez23 <88009412+tomasfranquez23@users.noreply.github.com>
2022-09-27 21:03:12 -0300

    Interactuando con HTML

---
+++ products.js
@@ -0,0 +1,53 @@
+const allProductos = [
+    { id : 1,
+      nombre: 'Nike Air Max', 
+      precio: 250,
+      categoria : "hombre",
+      img: "../img/h1.png"
+    },
+    
+    { id : 2,
+      nombre: 'Nike Grises',
+      precio: 300,
+      categoria : "hombre",
+      img: "../img/h2.png"
+    },
+    { id : 3,
+      nombre: 'Botita Adidas', 
+      precio: 300,
+      categoria : "hombre",
+      img: "../img/h3.png"
+    },
+    
+    { id : 4,
+      nombre: 'Buzo Puma',
+      precio: 100,
+      categoria : "hombre",
+      img: "../img/h4.png"
+    },
+    { id : 5,
+      nombre: 'Nike Negros',
+      precio: 170,
+      categoria : "hombre",
+      img: "../img/h5.png"
+    },
+    { id : 6,
+      nombre: 'Campera Adidas',
+      precio: 60,
+      categoria : "hombre",
+      img: "../img/h6.png"
+    },
+    { id : 7,
+      nombre: 'Buzo Adidas',
+      precio: 75,
+      categoria : "hombre",
+      img: "../img/h7.png"
+    },
+    { id : 8,
+      nombre: 'Championes Puma',
+      precio: 120,
+      categoria : "hombre",
+      img: "../img/h8.png"
+    },
+    
+    ]
