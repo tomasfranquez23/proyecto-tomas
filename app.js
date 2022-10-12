48eda18868e62a58856bbfa842f392280abb7777
tomasfranquez23 <88009412+tomasfranquez23@users.noreply.github.com>
2022-10-11 23:47:43 -0300

    agregando carrito de compras

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 
-//Productos
-
-const contenedorProductos = document.getElementById("contenedor-productos")
+const contenedorProductos = document.getElementById('contenedor-productos')
+const contenedorCarrito = document.getElementById('carrito-contenedor')
+const botonVaciar = document.getElementById('vaciar-carrito')
+const contadorCarrito = document.getElementById('contadorCarrito')
+const cantidad = document.getElementById('cantidad')
+const precioTotal = document.getElementById('precioTotal')
+const cantidadTotal = document.getElementById('cantidadTotal')
 
 let carrito = []
 
-//Agrgando Porductos al Html, Funcionalidad Boton carrito
+document.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem('carrito')){
+        carrito = JSON.parse(localStorage.getItem('carrito'))
+        actualizarCarrito()
+    }
+})
+botonVaciar.addEventListener('click', () => {
+    carrito.length = 0
+    actualizarCarrito()
+})
 
-allProductos.forEach(producto => {
+//INYECTA EL HTML
+stockProductos.forEach(producto => {
   const div = document.createElement("div")
   div .classList.add("col")
   div.innerHTML = `
@@ -25,17 +39,74 @@ allProductos.forEach(producto => {
   
   contenedorProductos.appendChild(div)
 
-  const boton = document.getElementById(`agregar${producto.id}`)
-  boton.addEventListener("click", () => {
-    agregarAlCarrito(producto.id)
-  })
-  
-});
+    const boton = document.getElementById(`agregar${producto.id}`)
 
+    boton.addEventListener('click', () => {
+       //esta funcion ejecuta el agregar el carrito con la id del producto
+        agregarAlCarrito(producto.id)
+    })
+})
+
+
+//AGREGAR AL CARRITO
 const agregarAlCarrito = (prodId) => {
-  const item = allProductos.find((prod) => prod.id === prodId)
-  carrito.push(item)
-  console.log(carrito)
-}
-  
 
+    //PARA AUMENTAR LA CANTIDAD Y QUE NO SE REPITA
+    const existe = carrito.some (prod => prod.id === prodId) //comprobar si el elemento ya existe en el carro
+
+    if (existe){ //SI YA ESTÁ EN EL CARRITO, ACTUALIZAMOS LA CANTIDAD
+        const prod = carrito.map (prod => {
+            // map encuentre cual es el q igual al que está agregado, le suma la cantidad
+            if (prod.id === prodId){
+                prod.cantidad++
+            }
+        })
+    } else { //EN CASO DE QUE NO ESTÉ,SE AGREGA AL CARRITO
+        const item = stockProductos.find((prod) => prod.id === prodId)
+        carrito.push(item)
+    }
+    actualizarCarrito()
+}
+
+const eliminarDelCarrito = (prodId) => {
+    const item = carrito.find((prod) => prod.id === prodId)
+
+    const indice = carrito.indexOf(item) 
+    carrito.splice(indice, 1) 
+   
+    actualizarCarrito() 
+    console.log(carrito)
+}
+
+const actualizarCarrito = () => {
+  
+   
+    contenedorCarrito.innerHTML = "" 
+    carrito.forEach((prod) => {
+        const div = document.createElement('div')
+        div.className = ('productoEnCarrito')
+        div.innerHTML = `
+        <p>${prod.nombre}</p>
+        <p>Precio:$${prod.precio}</p>
+        <p>Cantidad: <span id="cantidad">${prod.cantidad}</span></p>
+        <button onclick="eliminarDelCarrito(${prod.id})" class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>
+        `
+
+        contenedorCarrito.appendChild(div)
+        
+        localStorage.setItem('carrito', JSON.stringify(carrito))
+
+    })
+
+    contadorCarrito.innerText = carrito.length 
+
+    console.log(carrito)
+    precioTotal.innerText = carrito.reduce((acc, prod) => acc + prod.cantidad * prod.precio, 0)
+   
+}
+
+
+
+  
+  
+  
